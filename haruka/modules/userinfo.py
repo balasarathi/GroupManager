f8721d3da493d7c1f4319c06ea216eea4e6580dc
balasarathi <75534391+balasarathi@users.noreply.github.com>
2021-01-03 12:43:38 +0530

    Update userinfo.py

--- haruka/modules/userinfo.py
+++ haruka/modules/userinfo.py
@@ -143,3 +143,15 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
+HELP_STRINGS = """
+
+Hello! my name *{}*.
+
+*Main* commands available:
+ - /start: start the bot
+ - /help: PM's you this message.
+ - /help <module name>: PM's you info about that module.
+ - /donate: information about how to donate!
+ - /settings:
+   - in PM: will send you your settings for all supported modules.
+   - in a group: will redirect you to pm, with all that chat's settings.
